53080f4c72e005bba7be13a4dcbb6f32d453c3bd
sann <psang4845@gmail.com>
2023-03-21 02:20:36 +0700

    ko co gi thay doi

--- App.js
+++ App.js
@@ -2,13 +2,11 @@ import React, { useEffect, useLayoutEffect } from "react";
 import { Provider } from "react-redux";
 import Main from "./src/Main";
 import store from "./src/redux";
-import * as SecureStore from 'expo-secure-store';
-import axios from 'axios';
+import * as SecureStore from "expo-secure-store";
+import axios from "axios";
 
-
-import { withExpoSnack } from 'nativewind';
+import { withExpoSnack } from "nativewind";
 const App = () => {
-
   return (
     <>
       <Provider store={store}>
